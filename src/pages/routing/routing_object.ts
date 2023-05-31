696b09df11fa32695b9b2d65b4d90fe910cf717f
tyankatsu0105 <frips.ryilsufupe+dev@gmail.com>
2023-05-31 10:13:40 +0900

    update

--- src/pages/routing/routing_object.ts
+++ src/pages/routing/routing_object.ts
@@ -6,7 +6,7 @@ export type RoutingObject = {
    * '/users/:userID'
    * '/users/:userID/:postID'
    */
-  readonly pathname: string
+  readonly pathname: string;
   /**
    * query parameterを利用する場合に指定する
    */
@@ -17,7 +17,7 @@ export type RoutingObject = {
      * '/users?userCategory=admin'
      * // => 'userCategory'
      */
-    key: string
+    key: string;
     /**
      * valueとして入ってくる可能性のある値を指定する
      * 型のstring literal unionを作るために利用するため、実際に値として利用されない
@@ -27,9 +27,9 @@ export type RoutingObject = {
      * '/users?userCategory=admin'
      * // => [ 'admin', 'general' ]
      */
-    expectedValues?: readonly string[]
-  }[]
-}
+    expectedValues?: readonly string[];
+  }[];
+};
 
 /**
  * path parameter含む文字列から、path parameterにあたる文字列の string literal union を生成する
@@ -38,39 +38,45 @@ export type RoutingObject = {
  * type Result = PathParams<'/users/:userID/:postID'>
  * // type Result = 'userID' | 'postID'
  */
-export type PathParams<Path extends string> = Path extends `:${infer Param}/${infer Rest}`
-  ? Param | PathParams<Rest>
-  : Path extends `:${infer Param}`
-  ? Param
-  : Path extends `${any}:${infer Param}`
-  ? PathParams<`:${Param}`>
-  : never
+export type PathParams<Path extends string> =
+  Path extends `:${infer Param}/${infer Rest}`
+    ? Param | PathParams<Rest>
+    : Path extends `:${infer Param}`
+    ? Param
+    : Path extends `${any}:${infer Param}`
+    ? PathParams<`:${Param}`>
+    : never;
 
-export const getReplacedPath = (params: { pathname: RoutingObject['pathname']; path: Record<string, string> }) => {
-  const pathEntries = Object.entries(params.path)
-  const hasPathParameter = pathEntries.length > 0
+export const getReplacedPath = (params: {
+  pathname: RoutingObject["pathname"];
+  path: Record<string, string>;
+}) => {
+  const pathEntries = Object.entries(params.path);
+  const hasPathParameter = pathEntries.length > 0;
 
-  if (!hasPathParameter) return params.pathname
+  if (!hasPathParameter) return params.pathname;
   return Object.entries(params.path).reduce((acc, [key, value]) => {
-    if (typeof value !== 'string') return acc
+    if (typeof value !== "string") return acc;
 
-    return acc.replace(`:${key}`, value)
-  }, params.pathname)
-}
+    return acc.replace(`:${key}`, value);
+  }, params.pathname);
+};
 
 export const getQueryString = (params: { query: Record<string, unknown> }) => {
-  const queryEntries = Object.entries(params.query)
-  const hasQueryParameter = queryEntries.length > 0
+  const queryEntries = Object.entries(params.query);
+  const hasQueryParameter = queryEntries.length > 0;
 
-  if (!hasQueryParameter) return ''
+  if (!hasQueryParameter) return "";
 
-  const queryString = queryEntries.map(([key, value]) => `${key}=${value}`).join('&')
+  const queryString = queryEntries
+    .map(([key, value]) => `${key}=${value}`)
+    .join("&");
 
-  return `?${queryString}`
-}
+  return `?${queryString}`;
+};
 
 /**
- * NOTE: satisfiesが使えない場合、この関数を用いてrouting objectを生成する。
+ * routingに関するオブジェクトを生成する
  * - 引数のオブジェクトは必ずconst assertionすること。
  *
  * @example
@@ -88,11 +94,15 @@ export const getQueryString = (params: { query: Record<string, unknown> }) => {
  *   ]
  * } as const)
  */
-export const createRoutingObject = <Pathname extends RoutingObject['pathname'], QueryParameters extends RoutingObject['queryParameters']>(routingObject: {
-  pathname: Pathname
-  queryParameters: QueryParameters
+export const createRoutingObject = <
+  Pathname extends RoutingObject["pathname"],
+  QueryParameters extends RoutingObject["queryParameters"]
+>(routingObject: {
+  pathname: Pathname;
+  queryParameters: QueryParameters;
 }) => {
-  const queryParameterKeys: QueryParameters[number]['key'][] = routingObject.queryParameters.map(queryParameter => queryParameter.key)
+  const queryParameterKeys: QueryParameters[number]["key"][] =
+    routingObject.queryParameters.map((queryParameter) => queryParameter.key);
   /**
    * pathを生成する
    * @example
@@ -123,30 +133,33 @@ export const createRoutingObject = <Pathname extends RoutingObject['pathname'], 
    */
   const generatePath = (parameters: {
     query: {
-      [Key in QueryParameters[number]['key']]?: Extract<QueryParameters[number], { key: Key }> extends {
-        key: Key
-        expectedValues?: readonly (infer ExpectedValues)[]
+      [Key in QueryParameters[number]["key"]]?: Extract<
+        QueryParameters[number],
+        { key: Key }
+      > extends {
+        key: Key;
+        expectedValues?: readonly (infer ExpectedValues)[];
       }
         ? ExpectedValues | (string & {})
-        : string
-    }
+        : string;
+    };
     path: {
-      [key in PathParams<Pathname>]: string
-    }
+      [key in PathParams<Pathname>]: string;
+    };
   }) => {
     const replacedPath = getReplacedPath({
       pathname: routingObject.pathname,
-      path: parameters.path
-    })
+      path: parameters.path,
+    });
 
-    const queryString = getQueryString({ query: parameters.query })
+    const queryString = getQueryString({ query: parameters.query });
 
-    return `${replacedPath}${queryString}`
-  }
+    return `${replacedPath}${queryString}`;
+  };
 
   return {
     pathname: routingObject.pathname,
     queryParameterKeys,
-    generatePath
-  }
-}
+    generatePath,
+  };
+};
